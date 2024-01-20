0bf81f25c624bcd888f2f2ca40f0653397d7abf0
Alice A <alice.anglesjo@hotmail.com>
2024-01-20 20:29:14 +0100

    fix: image border radius

--- src/components/ImageGrid.tsx
+++ src/components/ImageGrid.tsx
@@ -23,7 +23,7 @@ export const ImageGrid = () => {
             ? images.map((image, index) => (
                 <div
                   className={classNames(
-                    "aspect-square bg-gray-200 rounded-lg overflow-hidden  col-span-1 md:col-span-2 relative"
+                    "aspect-square bg-gray-200 overflow-hidden rounded col-span-1 md:col-span-2 relative"
                   )}
                   key={image.id}
                 >
@@ -39,7 +39,7 @@ export const ImageGrid = () => {
             : [0, 1, 2, 3, 4].map((index) => <SkeletonImage key={index} />)}
 
           {images ? (
-            <div className=" p-4 h-full w-full bg-palette-dark aspect-square rounded-lg overflow-hidden  col-span-1 md:col-span-2 ">
+            <div className="  rounded p-4 h-full w-full bg-palette-dark aspect-square overflow-hidden  col-span-1 md:col-span-2 ">
               <div className="flex justify-center items-center h-full w-full uppercase">
                 <div className="space-y-6 flex flex-col items-center justify-center">
                   <h3 className="text-white text-center text-6xl font-semibold">
--- src/components/SkeletonImage.tsx
+++ src/components/SkeletonImage.tsx
@@ -3,8 +3,8 @@ import { FaImage } from "react-icons/fa";
 
 export const SkeletonImage = () => {
   return (
-    <div className=" p-4 h-full w-full bg-gray-200 aspect-square rounded-lg overflow-hidden  col-span-1 md:col-span-2 ">
-      <div className="flex justify-center items-center h-full w-full border animate-pulse rounded-lg">
+    <div className=" p-4 h-full w-full bg-gray-200 aspect-square  rounded overflow-hidden  col-span-1 md:col-span-2 ">
+      <div className="flex justify-center items-center h-full w-full border animate-pulse  rounded">
         <FaImage color="grey" size={100} />
       </div>
     </div>
